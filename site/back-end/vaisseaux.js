24b8e1efb114c85d8bc7f771d02c40bbbe27cb3d
Henry <benjamin.henry@edu.esiee-it.fr>
2023-01-03 12:11:17 +0100

    Tires de vaisseau

--- site/back-end/vaisseaux.js
+++ site/back-end/vaisseaux.js
@@ -1,5 +1,21 @@
-class Vaisseaux{
+let lastShotTime = 0;
+let shotDelay = 1000;
 
-    constructor
+let bullets = [];
 
-}
+function updateGame() {
+    for (let i = 0; i < bullets.length; i++) {
+        bullets[i].x += bullets[i].vx;
+        bullets[i].y += bullets[i].vy;
+      }
+    }
+    for (let i = 0; i < bullets.length; i++) {
+        for (let j = 0; j < enemies.length; j++) {
+          if (bullets[i].x >= enemies[j].x && bullets[i].x <= enemies[j].x + enemies[j].width &&
+                bullets[i].y >= enemies[j].y && bullets[i].y <= enemies[j].y + enemies[j].height) {
+            showImpact(enemies[j].x, enemies[j].y);
+            enemies.splice(j, 1);
+            bullets.splice(i, 1);
+          }
+        }
+    }
