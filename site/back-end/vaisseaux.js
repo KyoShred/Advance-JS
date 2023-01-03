d29027f77f93ebd226da007667d837e68ba1d9cb
leo <leoyng95@gmail.com>
2023-01-03 22:58:34 +0100

    Grille

--- site/back-end/vaisseaux.js
+++ site/back-end/vaisseaux.js
@@ -1,55 +1,16 @@
-/*const alienInvaders = [
-    0,1,2,3,4,5,6,7,8,9,
-    15,16,17,18,19,20,21,22,23,24,
-    30,31,32,33,34,35,36,37,38,39
-  ]
-*/
+const grid = document.querySelector('.grille');
 
-
-if (squares[currentShooterIndex].classlist.contains('invader','shooter')) {
-    resultDisplay.innerHTML = 'game over'
-    clearInterval(invadersId)
-
+for (let i = 0; i < 260 ; i++){
+    const squares = document.createElement('div')
+    grid.appendChild(squares)
 }
 
-for (let i = 0; 1 < alienInvaders.length; i++) {
-    if (alienInvaders[i] > squares.length){
-        resultDisplay.innerHTML = 'game over'
-        clearInterval(invadersId)
-    }
-}
-
-invadersId = setInterval(moveInvaders, 200);
 
 
 
 
-/*let playerDiv = document.getElementById("player");
-let enemyDiv = document.getElementById("enemy");
 
 
-let playerX = playerDiv
-let playerY = playerDiv
-let enemyX = enemyDiv
-let enemyY = enemyDiv
 
 
-function checkCollision() {
 
-
-if (playerX == enemyX && playerY == enemyY){
-    console.log("game over !");
-    }
-}
-
-setInterval(checkCollision,200);
-
-
- 
-if (playerX === enemyX && playerY === enemyY) {}
-  
-if (playerX === enemyX && playerY === enemyY) {}
-     
-if (playerX === enemyX && playerY === enemyY) {}
-    
-if (playerX === enemyX && playerY === enemyY) {}*/
