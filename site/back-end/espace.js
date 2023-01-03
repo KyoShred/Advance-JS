e3d8f240d9f1f36c02c3e95da6637121c06214d3
sacha <sacha.harel2@gmail.com>
2023-01-03 15:55:19 +0100

    Tentative de placement d'ennemis dans la grille

--- site/back-end/espace.js
+++ site/back-end/espace.js
@@ -18,16 +18,24 @@ class Espace {
     }
 
     crea_ennemi() {
-      
+      if (this.table != undefined){
+        let first_placement = Math.floor((this.nb_vais - 12) / 2); // utiliser Math.floor pour arrondir à l'entier inférieur
+        for (let i = 0; i < 3; i++) {
+          for (let j = 0; j < 12; j++) {
+            this.table[i][j + first_placement] = 10 // remplacer les valeurs de l'objet "table" directement          }
+          }
+        }
+      }
     }
-
+    
     aff_tab() {
-      for (let i = 0; i < this.nb_lignes; i++){
-        document.write(this.crea_grid()[i] + "<br>");
+      for (let i = 0; i < this.nb_lignes; i++) {
+        document.write(this.crea_grid()[i] + "<br>"); // Mise à la ligne de toutes les lignes du tableau à l'affichage
       }
     }
   }
   
-  let espace = new Espace(20, 14, 0);
+  let espace = new Espace(20, 14, 0); // Initialisation de la grille d'espace
 
+espace.crea_ennemi();
 espace.aff_tab();
