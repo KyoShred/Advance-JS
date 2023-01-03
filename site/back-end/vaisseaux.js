49fb608c2b723990e40c236447eb7f40994650c4
Henry <benjamin.henry@edu.esiee-it.fr>
2023-01-03 14:46:21 +0100

    vaisseau tire 2

--- site/back-end/vaisseaux.js
+++ site/back-end/vaisseaux.js
@@ -1,4 +1,4 @@
-let lastShotTime = 0;
+/*let lastShotTime = 0;
 let shotDelay = 1000;
 
 let bullets = [];
@@ -18,4 +18,31 @@ function updateGame() {
             bullets.splice(i, 1);
           }
         }
+    }*/
+
+function shoot(e) {
+    let laserId
+    let currentLaserIndex = currentShooterIndex
+    function moveLaser() {
+      squares[currentLaserIndex].classList.remove('laser')
+      currentLaserIndex -= width
+      squares[currentLaserIndex].classList.add('laser')
+      
+      if (squares[currentLaserIndex].classList.contains('invader')) {
+        squares[currentLaserIndex].classList.remove('laser')
+        squares[currentLaserIndex].classList.remove('invader')
+        squares[currentLaserIndex].classList.add('boom')
+      
+        setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
+        clearInterval(laserId)
+      
+        const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+        aliensRemoved.push(alienRemoved)
+        results++
+        resultsDisplay.innerHTML = results
+        console.log(aliensRemoved)
+      
+      }
+  
     }
+}
