b8d5f88a13ac57f0305069a6c096e48997680c3a
sacha <sacha.harel2@gmail.com>
2023-01-04 10:51:41 +0100

    grid presque finie parce qu'il y a encore des espaces entre les lignes

--- site/espace.js
+++ site/espace.js
@@ -1,24 +1,21 @@
 function crea_grid(){
+  let incr = 0
   for (let i = 0; i < 240; i++){
-    let incr = 0
-      var T = document.createElement('div');
-      T.classList.add('grid-cell');
-      if (incr === 0) {
-        T.classList.add('start');
-        incr += 1;
-      }
-      if (incr === 19) {
-        T.classList.add('end');
-        incr = 0
-      }
-      else {
-        incr += 1;
-      }
-      document.getElementById('grid-container').appendChild(T);
-      if (T.classList.contains('end')) {
-        var br = document.createElement('br');
-        document.getElementById('grid-container').appendChild(br);
-      }
+    var T = document.createElement('div');
+    T.classList.add('grid-cell');
+    if (incr === 0) {
+      T.classList.add('start');
+    }
+    if (incr === 19) {
+      T.classList.add('end');
+    }
+    document.getElementById('grid-container').appendChild(T);
+    incr += 1;
+    if (incr == 20) {
+      var br = document.createElement('br');
+      document.getElementById('grid-container').appendChild(br);
+      incr = 0
+    }
   }
 }
 
