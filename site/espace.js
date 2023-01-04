9aad8b7772ecc881c3e4845e34d99d8d5c00b13f
sacha <sacha.harel2@gmail.com>
2023-01-04 10:26:34 +0100

    apparition de la grille sur un bouton

--- site/espace.js
+++ site/espace.js
@@ -1,21 +1,18 @@
-var game = document.getElementsByClassName('grille');
-
-class Espace {
-  constructor(nb_lignes, nb_vais, pvs) {
-    this.nb_lignes = nb_lignes;
-    this.nb_vais = nb_vais;
-    this.pvs = pvs;
-    this.table = [];
-    this.gauche;
-    this.droite;
-    this.game_over;
+function crea_grid(){
+  for (let i = 0; i < 240; i++){
+      var T = document.createElement('div');
+      T.classList.add('grid-cell');
+      document.getElementById('grid-container').appendChild(T);
+      if ((i + 1) % 10 === 0) {
+        var br = document.createElement('br');
+        br.classList.add('no-margin');  // add class to remove margin
+        document.getElementById('grid-container').appendChild(br);
+      }
   }
+}
 
-  crea_grid() {
-    for (let i = 0; i < 240; i++){
-        var T = document.createElement('div');
-        T.classList.add('grid-cell');
-        document.getElementById('grid-container').appendChild(T);
-    }
+function aliens(){
+  for (let i = 0; i < 53; i++){
+    
   }
 }
