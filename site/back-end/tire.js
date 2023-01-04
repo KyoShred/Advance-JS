5f578192e5f2fdc966fe8498efc7ef37d47446ca
Henry <benjamin.henry@edu.esiee-it.fr>
2023-01-04 14:33:06 +0100

    tire

--- site/back-end/tire.js
+++ site/back-end/tire.js
@@ -1,48 +1,16 @@
-/*let lastShotTime = 0;
-let shotDelay = 1000;
+let laser = [];
+let boom;
 
-let bullets = [];
-
-function updateGame() {
-    for (let i = 0; i < bullets.length; i++) {
-        bullets[i].x += bullets[i].vx;
-        bullets[i].y += bullets[i].vy;
-      }
+function shooted() {
+  laser.forEach(tire => console.log(tire));{
+    if(laser == alien){
+    console.log('boom');}
+    
+    else {
     }
-    for (let i = 0; i < bullets.length; i++) {
-        for (let j = 0; j < enemies.length; j++) {
-          if (bullets[i].x >= enemies[j].x && bullets[i].x <= enemies[j].x + enemies[j].width &&
-                bullets[i].y >= enemies[j].y && bullets[i].y <= enemies[j].y + enemies[j].height) {
-            showImpact(enemies[j].x, enemies[j].y);
-            enemies.splice(j, 1);
-            bullets.splice(i, 1);
-          }
-        }
-    }*/
+  }
+}
 
-function shoot(e) {
-    let laserId
-    let currentLaserIndex = currentShooterIndex
-    function moveLaser() {
-      squares[currentLaserIndex].classList.remove('laser')
-      currentLaserIndex -= width
-      squares[currentLaserIndex].classList.add('laser')
-      
-      if (squares[currentLaserIndex].classList.contains('invader')) {
-        squares[currentLaserIndex].classList.remove('laser')
-        squares[currentLaserIndex].classList.remove('invader')
-        squares[currentLaserIndex].classList.add('boom')
-      
-        setTimeout(()=> squares[currentLaserIndex].classList.remove('boom'), 300)
-        clearInterval(laserId)
-      
-        const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
-        aliensRemoved.push(alienRemoved)
-        results++
-        resultsDisplay.innerHTML = results
-        console.log(aliensRemoved)
-      
-      }
-  
-    }
+function bullet(){
+
 }
