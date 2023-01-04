60adaf51d63c56c968b506aa6d19ed0df26bfd24
sacha <sacha.harel2@gmail.com>
2023-01-04 14:40:53 +0100

    Commit ajout du vaisseau

--- site/espace.js
+++ site/espace.js
@@ -1,5 +1,6 @@
 function crea_grid(){
   let incr = 0
+  let incrVaiss = 0
   for (let i = 0; i < 441; i++){
     var T = document.createElement('div');
     T.classList.add('grid-cell');
@@ -9,11 +10,12 @@ function crea_grid(){
     if (incr === 20) {
       T.classList.add('end'); // Ajout de la classe End à chaque fin de ligne
     }
-    if (incr === 409) {
-      T.classList.add('vaisseau'); // Ajout de la classe Vaisseau à la 409è incémentation
+    if (incrVaiss === 409) {
+      T.classList.add('vaisseau'); // Ajout de la classe Vaisseau à la 409è incrémentation
     }
     document.getElementById('grid-container').appendChild(T);
     incr += 1;
+    incrVaiss +=1
     if (incr == 21) {
       var br = document.createElement('br');
       document.getElementById('grid-container').appendChild(br);
