6f726068f43cd51900a073dd84e115a0787a9bc0
KyoShred <lucas.salaun2@gmail.com>
2023-01-04 10:25:25 +0100

    update

---
+++ app-1.js
@@ -0,0 +1,86 @@
+window.onload = startGame;
+var myGamePiece;
+
+function startGame(){
+    myGameArea.start();
+    myGamePiece = new Player(30, 30, "red", 195, 360);
+    
+}
+var myGameArea = {
+    container: document.createElement("div"),
+    start: function() {
+        this.container.style.width = "390px";
+        this.container.style.height = "390px";
+        this.container.style.border = "1px solid black";
+        document.body.insertBefore(this.container, document.body.childNodes[0]);
+        // other code
+    },
+    clear: function() {
+        // no need to clear the div element, just remove all child elements
+        while (this.container.firstChild) {
+        this.container.removeChild(this.container.firstChild);
+        }
+    }
+};
+function Player(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.limit = false;
+    this.moving = false;  // new flag to track if the player is moving or not
+    this.update = function() {
+        // create a new div element
+        this.div = document.createElement("div");
+        this.div.style.width = this.width + "px";
+        this.div.style.height = this.height + "px";
+        this.div.style.backgroundColor = this.color;
+        this.div.style.position = "absolute";
+        this.div.style.left = this.x + "px";
+        this.div.style.top = this.y + "px";
+        // append the div element to the game container
+        myGameArea.container.appendChild(this.div);
+        };      
+        this.newPos = function() {
+        if (this.moving) return;  // return early if the player is already moving
+        // move the game piece in a square-by-square imitation
+        this.x += this.speedX;
+        this.y += this.speedY;
+        // add a margin around the edge of the game area
+        const margin = 30;
+        if (this.x < margin) {
+        this.x = margin;
+        } else if (this.x > myGameArea.canvas.width - this.width - margin) {
+        this.x = myGameArea.canvas.width - this.width - margin;
+        }
+        if (this.y < (myGameArea.canvas.height*70)/100) {
+        this.y = (myGameArea.canvas.height*70)/100;
+        } else if (this.y > myGameArea.canvas.height - this.height - margin) {
+        this.y = myGameArea.canvas.height - this.height - margin;
+        }
+        // set the moving flag to true
+        this.moving = true;
+        // after a short delay, set the moving flag to false
+        setTimeout(() => this.moving = false, 200);  // delay in milliseconds
+    }
+}
+function updateGameArea() {
+    if (myGameArea.key && myGameArea.key === 'ArrowLeft') {
+        myGamePiece.div.style.left = myGamePiece.x + "px";
+    }
+    if (myGameArea.key && myGameArea.key === 'ArrowRight') {
+        myGamePiece.div.style.right = myGamePiece.x + "px";
+
+    }
+    if (myGameArea.key && myGameArea.key === 'ArrowUp') {
+        myGamePiece.div.style.top = myGamePiece.y + "px";
+    }
+    if (myGameArea.key && myGameArea.key === 'ArrowDown') {
+        myGamePiece.div.style.down = myGamePiece.y + "px";
+    }
+    myGamePiece.newPos(); 
+    myGamePiece.update();
+}
--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-function StartGame(){
+window.onload = startGame;
+
+var myGamePiece;
+
+function startGame(){
     myGameArea.start();
+    myGamePiece = new Player(30, 30, "red", 195, 360);
+    
 }
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = (500);
-        this.canvas.height = 300;
+        this.canvas.width = 390;
+        this.canvas.height = 390;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
@@ -18,46 +24,65 @@ var myGameArea = {
     }, 
     clear : function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }
-    
+  }   
 }
-
-function component (width, height, x, y){
+function Player(width, height, color, x, y) {
     this.width = width;
     this.height = height;
-    this.speed = 0;
+    this.speedX = 0;
+    this.speedY = 0;
     this.x = x;
     this.y = y;
-    this.update = function(){
-        ctx = myGameArea.context;
-        ctx.fillRect(this.x, this.width);
-    }
+    this.color = color;
+    this.limit = false;
+    this.moving = false;  // new flag to track if the player is moving or not
+    this.update = function() {
+      ctx = myGameArea.context;
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    };
     this.newPos = function() {
-        this.x += this.speedX;
+      if (this.moving) return;  // return early if the player is already moving
+      // move the game piece in a square-by-square imitation
+      this.x += this.speedX;
+      this.y += this.speedY;
+      // add a margin around the edge of the game area
+      const margin = 30;
+      if (this.x < margin) {
+        this.x = margin;
+      } else if (this.x > myGameArea.canvas.width - this.width - margin) {
+        this.x = myGameArea.canvas.width - this.width - margin;
+      }
+      if (this.y < (myGameArea.canvas.height*70)/100) {
+        this.y = (myGameArea.canvas.height*70)/100;
+      } else if (this.y > myGameArea.canvas.height - this.height - margin) {
+        this.y = myGameArea.canvas.height - this.height - margin;
+      }
+      // set the moving flag to true
+      this.moving = true;
+      // after a short delay, set the moving flag to false
+      setTimeout(() => this.moving = false, 200);  // delay in milliseconds
     }
-} 
-function updateGameArea(){
+  }
+function updateGameArea() {
     myGameArea.clear();
-    myGameArea.newPos();
-    myGameArea.update();
-}
-let myGamePiece = new component(30, 30, 10, 10);
-
-function moveleft() {
-    myGamePiece.speedX -= 1;
-}
-  
-function moveright() {
-    myGamePiece.speedX += 1;
-}
-function stopMove() {
     myGamePiece.speedX = 0;
-}
-document.addEventListener('keydown', function(e) {
-    if (e.key === 'ArrowLeft') {
-      moveleft();
-    } else if (e.key === 'ArrowRight') {
-      moveright();
+    myGamePiece.speedY = 0; 
+    if (myGameArea.key && myGameArea.key === 'ArrowLeft') {
+      myGamePiece.speedX = -30;
     }
-  });
-  StartGame();
+    if (myGameArea.key && myGameArea.key === 'ArrowRight') {
+      myGamePiece.speedX = 30;
+    }
+    if (myGameArea.key && myGameArea.key === 'ArrowUp') {
+      myGamePiece.speedY = -30;
+    }
+    if (myGameArea.key && myGameArea.key === 'ArrowDown') {
+      myGamePiece.speedY = 30;
+    }
+    myGamePiece.newPos(); 
+    myGamePiece.update();
+}
+
+
+
