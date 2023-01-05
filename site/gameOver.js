152c5bb9434422abbaa182890593197b71b047ac
leo <leoyng95@gmail.com>
2023-01-05 14:51:31 +0100

    gameOver

--- site/gameOver.js
+++ site/gameOver.js
@@ -1,27 +1,70 @@
 const endScreen = document.querySelectorAll('.endScreen') || { innerHTML: '' };
+/*const gameOverr = document.querySelector('.gameOver');
 
 
-let alien = [230];
- 
+
+function endgame(){
+gameOverr.style.left = '50%';
+gameOverr.style.right = '50%';
+gameOverr.style.display = 'block';
+}
+
+function startGame() {
+  gameOverr.style.display = 'none';
+}
+
+let gameO = false;
+
+function egame(){
+  gameO = true;
+  endgame();
+}
+
+function restartGame(){
+  gameO = false; 
+  startGame();
+}
+
+const gameloop = setInterval(() =>{
+  if (gameO){
+    clearInterval(gameloop);
+  }
+},20);*/
+ function addScore(value) {
+  score += value;
+  scoreDisplay.innerHTML = "Score: " + score;
+}
+
+let score;
+const scoreDisplay = document.querySelector(".score")
+
+const alien = [
+ 240
+
+]
+
 let vaisseau = 230; 
 
-alien.forEach(lol => console.log(lol));{
-    if (alien > vaisseau){
-        console.log('gameOver');
-        document.write("gameOver");
+alien.forEach(lol => {
+    if (alien > 219){
+      console.log('gameOver');
+      document.getElementById('score').innerHTML = 'GAME OVER';
+      
     }
     else if (alien == vaisseau)
     {
         console.log('gameOver');
-        document.write("gameOver");
+        let text = document.querySelector("h3");
+        Text.innerHTML = "Game Over";  
+        //alert("gameOverbtrd");
         }
-      }
+      })
 
 
-const gameOver = () => {
-  endScreen.innerHTML = `<div class = 'gameOver'>
+/*const gameOver = () => {
+  endScreen.innerHTML = `<div class = 'gameOver'>Game over <br/>score
   </div>`;
   endScreen.style.visibility = 'visible';
   endScreen.style.opacité = '1';
 
-}
+}*/
