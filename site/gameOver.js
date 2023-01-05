cb5d0809e48912d5dd6419da4cef8af1d29fd599
leo <leoyng95@gmail.com>
2023-01-05 16:23:38 +0100

    ADD

--- site/gameOver.js
+++ site/gameOver.js
@@ -38,25 +38,23 @@ const gameloop = setInterval(() =>{
 let score;
 const scoreDisplay = document.querySelector(".score")
 
-const alien = [
- 240
-
-]
+let aliens = [25,26,27,28,29,30,31,32,33,34,35,36,37,
+  46,47,48,49,50,51,52,53,54,55,56,57,58,
+  67,68,69,70,71,72,73,74,75,76,77,78,79];
 
 let vaisseau = 230; 
 
 alien.forEach(lol => {
-    if (alien > 219){
+    if (aliens > 219){
       console.log('gameOver');
       document.getElementById('score').innerHTML = 'GAME OVER';
       
     }
-    else if (alien == vaisseau)
+    else if (aliens == vaisseau)
     {
         console.log('gameOver');
         let text = document.querySelector("h3");
         Text.innerHTML = "Game Over";  
-        //alert("gameOverbtrd");
         }
       })
 
