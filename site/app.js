4c9c2f3927bda1abbd7837725fd388502e319cd0
leo <leoyng95@gmail.com>
2023-01-05 22:12:33 +0100

    gameOver

--- site/app.js
+++ site/app.js
@@ -3,7 +3,7 @@ let direction = 'gauche';
 
 var aliens = [25,26,27,28,29,30,31,32,33,34,35,36,37,
 46,47,48,49,50,51,52,53,54,55,56,57,58,
-67,68,69,70,71,72,73,74,75,76,77,78,79];
+67,68,69,70,71,72,73,74,75,76,77,78,79 ];
 
 function crea_grid() {
   let incr = 0
@@ -138,3 +138,38 @@ function bullet_shot() {
       compteur -= 1;
   }
 }
+
+// gameOver ! ----------------------------------------------------------------------------
+
+
+let score = document.getElementById('score');
+const scoreDisplay = document.querySelector(".score")
+
+
+function addScore(value) {
+  score += value;
+  scoreDisplay.innerHTML = "Score: " + score;
+}
+
+
+//alert('gameOver')
+
+
+
+
+let vaisseau = 230; 
+
+aliens.forEach(lol => {
+    if (lol > 219){
+      console.log('gameOver');
+      console.log(score)
+      score.innerHTML = 'GAME OVER';
+      
+    }
+    else if (lol == vaisseau)
+    {
+        console.log('gameOver');
+        let text = document.querySelector("h3");
+        Text.innerHTML = "Game Over";  
+        }
+      })
