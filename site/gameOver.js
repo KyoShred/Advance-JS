2f19ebea9521ef66f766889bb69a9f036fe495e6
leo <leoyng95@gmail.com>
2023-01-05 18:45:39 +0100

    o

--- site/gameOver.js
+++ site/gameOver.js
@@ -44,7 +44,7 @@ let aliens = [25,26,27,28,29,30,31,32,33,34,35,36,37,
 
 let vaisseau = 230; 
 
-alien.forEach(lol => {
+aliens.forEach(lol => {
     if (aliens > 219){
       console.log('gameOver');
       document.getElementById('score').innerHTML = 'GAME OVER';
