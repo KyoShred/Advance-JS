0f2d7d7ccc33bae967b08de3dbd13abb817518f1
leo <leoyng95@gmail.com>
2023-01-06 09:11:55 +0100

    l

--- site/app.js
+++ site/app.js
@@ -3,7 +3,7 @@ let direction = 'gauche';
 
 var aliens = [25,26,27,28,29,30,31,32,33,34,35,36,37,
 46,47,48,49,50,51,52,53,54,55,56,57,58,
-67,68,69,70,71,72,73,74,75,76,77,78,79 ];
+67,68,69,70,71,72,73,74,75,76,77,78,79];
 
 function crea_grid() {
   let incr = 0
@@ -59,7 +59,7 @@ function alien_movement() {
     }
     else if (direction == 'droite') {
       aliens.nextElementSibling.classList.add("alien");
-      aliens.classList.remove("alien");
+      //aliens.classList.remove("alien");
     }
   });
 }
